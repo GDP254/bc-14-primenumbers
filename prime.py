f79aaa3b7d54c306713598fb3bde0b96505c9479
GDP254 <stvnkrs@gmail.com>
2017-01-17 10:59:49 +0300

    Change of isPrime funciton ot is_prime for consistency

--- prime.py
+++ prime.py
@@ -7,7 +7,7 @@ def prime_nums(n):
 		raise ValueError()
 	rslt = []
 	for x in range(0, n):
-		if isPrime(x):
+		if is_prime(x):
 			rslt.append(x)
 	return rslt
 
@@ -19,7 +19,7 @@ Date: N/A
 Code version: Pseudocode
 Availability: http://www.cs.uwc.ac.za/~jconnan/FirstYear/checkprime.txt 
 """
-def isPrime(num):
+def is_prime(num):
 	if num < 2:
 		return False # not prime
 	pfactor = 2
@@ -35,8 +35,8 @@ def isPrime(num):
 Asymptotic analysis
 	- The iteration within the prime_nums function with run n + 1 times. 
 	- The extra iteration detects the conditions necessary for exiting the loop.
-	- On each iteration, the function isPrime is called with the current value of x as an arguement.
-	- This arguement is used to determine the number of iterations within the the isPrime function.
+	- On each iteration, the function is_prime is called with the current value of x as an arguement.
+	- This arguement is used to determine the number of iterations within the the is_prime function.
 	- The code represents a nested loop which has a complexity of O(n^2) i.e. quadratic complexity.
 	- It is however unlikely that this worst scenario will occur since the nested loop is capped to the square root of the input.
 """
